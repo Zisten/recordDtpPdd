2c859b15e26a4d13c9d3b60bf29549159edb4d96
Nikita <127138657+Zisten@users.noreply.github.com>
2026-05-11 00:05:29 +0300

    Disable SchemaUtils auto-creation to avoid MySQL auto_increment/table definition conflicts

--- composeApp/src/jvmMain/kotlin/org/course/recorddtppdd/db/DatabaseFactory.kt
+++ composeApp/src/jvmMain/kotlin/org/course/recorddtppdd/db/DatabaseFactory.kt
@@ -2,34 +2,20 @@ package org.course.recorddtppdd.db
 
 import org.course.recorddtppdd.config.AppConfig
 import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.transactions.transaction
 
 /**
  * Фабрика подключения к базе данных.
  *
- * Использует Exposed ORM (DSL).
- * Подключение происходит через [Database.connect]:
- *   - url, user, password берутся из [AppConfig]
- *   - драйвер: com.mysql.cj.jdbc.Driver
- *
- * Все запросы выполняются внутри блока [transaction { }].
- *
- * Пример:
- * ```kotlin
- * DatabaseFactory.init()
- * val officers = transaction { Officers.selectAll().toList() }
- * ```
+ * ВАЖНО:
+ *  - В этом проекте схема БД создаётся/мигрируется SQL-скриптом (см. /sql в репозитории).
+ *  - Поэтому мы НЕ вызываем SchemaUtils.create* для MySQL, иначе Exposed попытается
+ *    "досоздать" таблицы/колонки по Tables.kt и может привести к конфликтам
+ *    (в т.ч. ошибке вида: "Incorrect table definition; there can be only one auto column...").
  */
 object DatabaseFactory {
 
     private var initialized = false
 
-    /**
-     * Инициализирует подключение к MySQL и создаёт таблицы, если они ещё не существуют.
-     * Вызывается один раз при старте приложения.
-     * @throws Exception если подключение к БД не удалось.
-     */
     fun init() {
         if (initialized) return
 
@@ -39,20 +25,6 @@ object DatabaseFactory {
             user = AppConfig.DB_USER,
             password = AppConfig.DB_PASSWORD
         )
-
-        // Создаём таблицы если их нет (SchemaUtils.createMissingTablesAndColumns)
-        transaction {
-            SchemaUtils.createMissingTablesAndColumns(
-                Officers,
-                Drivers,
-                Licenses,
-                Vehicles,
-                AccidentsRecords,
-                AccidentParticipants,
-                ViolationsTypes,
-                ViolationRecords
-            )
-        }
 
         initialized = true
     }
